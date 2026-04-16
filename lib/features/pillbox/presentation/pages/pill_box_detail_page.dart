9178e2c5fac3c15891b5bfdf169e780297292ca8
KODRIT\kodRit <rtcpr0546@gmail.com>
2026-04-17 06:08:49 +0700

    filter medication selection to user-added medicines only

--- lib/features/pillbox/presentation/pages/pill_box_detail_page.dart
+++ lib/features/pillbox/presentation/pages/pill_box_detail_page.dart
@@ -521,10 +521,10 @@ class _PillBoxDetailPageState extends State<PillBoxDetailPage> {
       builder: (context) => UnifiedSelectionDialog(
         userId: _userId!,
         title: 'เพิ่มยาลงในกล่อง',
-        showMasterMedications: true,
+        showMasterMedications: false,
         showBoxes: false,
-        allowFreeText: true,
-        loadAllMedicationsOnOpen: false,
+        allowFreeText: false,
+        loadAllMedicationsOnOpen: true,
       ),
     );
 
